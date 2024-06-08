9559b10b3a5c4de459bf60da04367b3635b15626
Afei <afeibaili@outlook.com>
2024-06-08 23:05:26 +0800

    优化主页

--- src/router/index.js
+++ src/router/index.js
@@ -6,34 +6,30 @@ import Test from "@/pages/Test.vue";
 Vue.use(VueRouter);
 
 
-export default new VueRouter({
-    routes: [{
-        path: "/router",
-        component: Routes,
-        children: [
-            {
-                name: "message", path: "message", component: Test,
-                props(r) {
+const router = new VueRouter({
+    routes: [
+        {
+            path: "/router", component: Routes, meta: {title: "列表主页"}, children: [{
+                name: "message", path: "message", component: Test, meta: {title: "消息"}, props(r) {
                     return {name: r.query.name}
                 }
-            },
-            {
-                name: "friend", path: "friend", component: Test,
-                props(r) {
+            }, {
+                name: "friend", path: "friend", component: Test, meta: {title: "好友"}, props(r) {
                     return {name: r.query.name}
                 }
-            },
-            {
-                name: "chatRoom", path: "chatRoom", component: Test,
-                props(r) {
+            }, {
+                name: "chatRoom", path: "chatRoom", component: Test, meta: {title: "聊天室"}, props(r) {
                     return {name: r.query.name}
                 }
-            },
-        ],
-        props(route) {
-            return {route: route.query.route};
-        }
-    },
-        {path: "/test", component: Test},
-    ]
+            },], props(route) {
+                return {route: route.query.route};
+            }
+        },
+        {path: "/test", component: Test, meta: {title: "测试"}},
+    ],
 })
+router.afterEach((to) => {
+    console.log("已加载")
+    document.title = to.meta.title || "欢迎"
+})
+export default router;
